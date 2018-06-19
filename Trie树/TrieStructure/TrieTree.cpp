be118d878a59ad28a51a94e52e903e9c03858e9d
DestinyXuyh <1303624575@qq.com>
2018-06-19 23:35:22 +0800

    trie树

---
+++ Trie树/TrieStructure/TrieTree.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <string>
+#include "TrieTree.h"
+using namespace std;
+
+void TrieTree::insert(const char* word)
+{
+
+}
+
+bool TrieTree::search(const char* word)
+{
+	
+
+}
+
+bool TrieTree::startWith(const char* prefix)
+{
+
+}
+
+void printAllString(vector<string>& word_list, string& word)
+{
+
+}
+
+int main()
+{
+	
+
+	system("pause");
+	return 0;
+}
